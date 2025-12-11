be343aa40e88e540b856c7c22cd27ce7dddcaf0d
unknown <ashanelson272@gmail.com>
2025-12-11 18:15:46 +0500

    Initial commit

---
+++ src/Program1.java
@@ -0,0 +1,9 @@
+public class Program1 {
+    public static void main(String[] args) {
+        int num1 = 9;
+        int num2 = 10;
+        int num3 = 5;
+
+        System.out.println("Sum is " + (num1 + num2 + num3));
+    }
+}
