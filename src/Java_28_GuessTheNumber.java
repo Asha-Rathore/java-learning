361ccbf9a719f52963802adfcd1229db90a8fbe5
unknown <ashanelson272@gmail.com>
2025-12-17 11:39:58 +0100

    Guess the number Game with OOP

---
+++ src/Java_28_GuessTheNumber.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class GuessTheNumberGame{
+    private int noOfGuesses;
+    private int userInput;
+    private int computerNo;
+
+    public GuessTheNumberGame(){
+        Random random = new Random();
+        computerNo = random.nextInt(200);
+    }
+
+    public void setNoOfGuesses(int n){
+        noOfGuesses = n;
+    }
+
+    public int getNoOfGuesses(){
+        return noOfGuesses;
+    }
+
+    public void takeUserInput(){
+        System.out.println("**************");
+        System.out.println("Guess a number");
+        Scanner sc = new Scanner(System.in);
+        userInput = sc.nextInt();
+    }
+
+    public boolean isCorrectNumber(){
+        while(computerNo != userInput){
+            if(computerNo > userInput){
+                System.out.println("Guess greater number");
+            }
+            else {
+                System.out.println("Guess smaller number");
+            }
+            return false;
+        }
+        return true;
+    }
+}
+
+public class Java_28_GuessTheNumber {
+    static void main(String[] args) {
+        GuessTheNumberGame guessTheNumberGame = new GuessTheNumberGame();
+        guessTheNumberGame.takeUserInput();
+        int score = 1;
+        while (!guessTheNumberGame.isCorrectNumber()){
+            System.out.println("Try again!");
+            guessTheNumberGame.takeUserInput();
+            score++;
+            guessTheNumberGame.setNoOfGuesses(score);
+        }
+        System.out.println("You won with " + guessTheNumberGame.getNoOfGuesses() + " score");
+    }
+}
